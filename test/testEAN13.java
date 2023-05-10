9873dde6d27c98acff73d5b3dcc585f7454291ff
kioshin33 <125193617+kioshin33@users.noreply.github.com>
2023-05-10 10:52:22 +0200

    completamento e prova dei test

--- test/testEAN13.java
+++ test/testEAN13.java
@@ -8,35 +8,36 @@ import org.junit.Test;
 public class testEAN13 {
 
     
-    private EAN13 EAN13;
+    private EAN13 ean ;
 
     @Before
     public void init(){
-        EAN13 = new EAN13();
+        ean = new EAN13();
     }
 
     @Test 
     public void testEanVerificaCodiceValido(){
-        EAN13.setEAN13("3614273613422");
-        assertTrue(EAN13.verificaCodice());
+        ean.setCodice("3614273613422");
+        assertTrue(ean.verificaCodice());
     }
 
     @Test
     public void testEanVerificaCodiceNonValido(){
-        EAN13.setEAN13("3614273613422");
-        assertFalse(EAN13.verificaCodice());
+        ean.setCodice("3614273613412");
+        assertFalse(ean.verificaCodice());
     }
 
     @Test
     public void testEanVerificaCodiceLunghezzaNonValida(){
-        EAN13.setEAN13("3614273613422");
-        assertFalse(EAN13.verificaCodice());
+        ean.setCodice("36142736134ffdsf22");
+        assertFalse(ean.verificaCodice());
     }
 
     @Test
     public void testEanVerificaCodiceVuoto(){
-        EAN13.setEAN13("3614273613422");
-        assertThrows(NullPointerException.class,null);    
+        assertThrows(NullPointerException.class, () ->{
+            ean.setCodice("3614273613422");
+        });    
     
     }
 
